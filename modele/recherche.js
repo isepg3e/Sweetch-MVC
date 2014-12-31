c5fcb98de6b04484033afffd27e235f916abfbad
bebec <jordanhirbec@yahoo.fr>
2014-12-31 14:59:18 +0100

    re

--- modele/recherche.js
+++ modele/recherche.js
@@ -22,8 +22,21 @@ function rechercheXML()
 
 
 data = "";
-	data = "checkbox_wifi=" + document.getElementById("checkbox_wifi").checked;
-	data = data + "&checkbox_tv=" + document.getElementById("checkbox_tv").checked;
+	//data = "checkbox_wifi=" + document.getElementById("checkbox_wifi").checked;
+	data = data + "checkbox_tv=" + document.getElementById("checkbox_tv").checked;
+	//data = data + "&checkbox_maison=" + document.getElementById("checkbox_maison").checked;
+	//data = data + "&checkbox_appartement=" + document.getElementById("checkbox_appartement").checked;
+	//data = data + "&checkbox_loft=" + document.getElementById("checkbox_loft").checked;
+	//data = data + "&checkbox_autre=" + document.getElementById("checkbox_autre").checked;
+//pour les listes deroulantes:
+var selectElmt = document.getElementById("region");
+var valeurselectionnee = selectElmt.options[selectElmt.selectedIndex].value;
+//var textselectionne = selectElmt.options[selectElmt.selectedIndex].text;
+	
+	data = data + "&region=" + valeurselectionnee;
+	
+	//data = data + "&ile_de_France=" + document.getElementById("ile_de_France").value;
+	
 	alert(data);
     xmlhttp.open("POST","Modele/recherche_post.php",true);
 	xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
--- modele/recherche_post.php
+++ modele/recherche_post.php
@@ -1,6 +1,7 @@
 <?php
 $checkboxwifi=$_POST['checkbox_wifi'];
 $checkboxtv=$_POST['checkbox_tv'];
+$region=$_POST['region'];
 
 // D'abord, je me connecte à la base de données!!!
 try
@@ -24,22 +25,36 @@ die('Erreur : '.$e->getMessage());
 				$requete = $requete . " AND tv = true";
 			}
 	}
-		
+	if( $region != '0') {
+			if ( $requete == "") {
+				
+				$requete = " WHERE region = '". $region . "' ";
+			}
+			else {
+				$requete = $requete . " AND region = '". $region ."' ";
+			}
+	}
+
 
 		// on peut aussi construit le where avec des conditions ... 
-		$req = $bdd->query('SELECT L.*, P.* FROM loge L INNER JOIN photo P ON (P.id_loge = L.id_loge) ' . $requete);
+		//echo 'SELECT L.*, P.* FROM loge L INNER JOIN photo P ON (P.id_loge = L.id_loge) ' . $requete;
+		$req = $bdd->query('SELECT L.*, P.* FROM loge L INNER JOIN photo P ON (P.id_loge = L.id_loge) ' . $requete );
+		//$req = $bdd->query('SELECT L.*, P.* FROM loge L INNER JOIN photo P ON (P.id_loge = L.id_loge) WHERE region = $_POST["region"]');
 		//$dateFormated = split('/', $_POST['age']);
 		//$date = $dateFormated[2].'-'.$dateFormated[1].'-'.$dateFormated[0];
 		//$req->execute(array('wifi' => true, 'tv' => false));
 		
 		while ($donnees = $req->fetch())
 		{
+
 			echo '<image src="Images/' . $donnees['chemin']  . '" />';
-			echo $donnees['ville'] ;
+			echo $donnees['ville'];
 			echo '<br />';
 		}
-				
-	//	echo "recherche=ok";
+	//echo print_r ($_POST);
+	//echo "<br />";			
+ //echo "SELECT L.*, P.* FROM loge L INNER JOIN photo P ON (P.id_loge = L.id_loge) WHERE region = '". $_POST['region'] ."' ";
+		
 
 $req->closeCursor();
 
